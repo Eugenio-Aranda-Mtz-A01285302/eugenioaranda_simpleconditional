53edc826930813e538c71b9a51a2e89582f85a04
Eugenio-Aranda-Mtz-A01285302 <70651900+Eugenio-Aranda-Mtz-A01285302@users.noreply.github.com>
2020-09-24 10:52:24 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+let celsius = 0
+basic.forever(function () {
+    celsius = input.temperature()
+    if (celsius == 10) {
+        basic.showNumber(celsius)
+        basic.showString("ski time")
+    } else if (celsius == 20) {
+        basic.showNumber(celsius)
+        basic.showString("Picnic time!")
+    } else if (celsius == 30) {
+        basic.showNumber(celsius)
+        basic.showString("Beach time!")
+    } else if (celsius == 40) {
+        basic.showNumber(celsius)
+        basic.showString("It is better to stay at home")
+    } else {
+        basic.showNumber(celsius)
+    }
+    basic.clearScreen()
+})
